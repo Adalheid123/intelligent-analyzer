2350bcaadb5eb86a32378bb7cf28f068c72e9188
Adalheid123 <18222911936@163.com>
2026-08-06 12:08:57 +0800

    Update app.py

--- analysis/app.py
+++ analysis/app.py
@@ -103,6 +103,3 @@ def analyze_teaching():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
